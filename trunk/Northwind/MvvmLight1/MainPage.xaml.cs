440c288878b58714268cc668844f7c689785f42d
shujaatned@gmail.com <shujaatned@gmail.com@7965f041-bd74-18c8-724d-cd0e13b3cbd4>
2011-03-16 08:17:33 +0000

    Recommendations for multi targeting (Silverlight and WPF)

---
+++ trunk/Northwind/MvvmLight1/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MvvmLight1
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
