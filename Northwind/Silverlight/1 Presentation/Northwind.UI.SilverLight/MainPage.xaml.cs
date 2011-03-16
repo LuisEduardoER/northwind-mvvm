ecf52329af46917e6b1c0e5fe9211c08936248ec
shujaatned <shujaatned@gmail.com>
2011-03-16 08:17:33 +0000

    Recommendations for multi targeting (Silverlight and WPF)

---
+++ Northwind/Silverlight/1 Presentation/Northwind.UI.SilverLight/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Northwind.UI.SilverLight
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
